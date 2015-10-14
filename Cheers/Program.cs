9ed399f4f4eb6b947fef63b5d09edfefccdcd908
britcorona <BrittneyLCorona@gmail.com>
2015-10-14 14:32:57 -0500

    Part1

--- Cheers/Program.cs
+++ Cheers/Program.cs
@@ -10,6 +10,26 @@ namespace Cheers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello there! What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+            name = name.ToLower();
+            foreach (char letter in name)
+            {
+                string aOrAn = "a";
+                foreach (char nonvoiced in "halfnorsemix")
+                {
+                    if (letter == nonvoiced)
+                    {
+                        aOrAn = "an";
+                    }
+                }
+                Console.WriteLine("Give me " + aOrAn + "... " + letter);
+            }
+            name = name.ToUpper();
+            Console.WriteLine(name + "'s just GRAND!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
