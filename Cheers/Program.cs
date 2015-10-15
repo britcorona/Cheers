697be58c877b8fd0562fd9a47c0ca04506711548
britcorona <BrittneyLCorona@gmail.com>
2015-10-15 14:08:38 -0500

    Cheers pt2

--- Cheers/Program.cs
+++ Cheers/Program.cs
@@ -13,6 +13,11 @@ namespace Cheers
             Console.WriteLine("Hello there! What's your name?");
             string name = Console.ReadLine();
             Console.WriteLine("Hi " + name);
+            Console.WriteLine("Hello, " + name + " what's your birthday? MM/DD");
+            string birthday = Console.ReadLine();
+            DateTime convertedBirthday = Convert.ToDateTime(birthday);
+            DateTime today = DateTime.Today;
+
             foreach (char letter in name.ToLower())
             {
                 if (Char.IsLetter(letter))
@@ -28,8 +33,23 @@ namespace Cheers
                     Console.WriteLine("Give me " + article + "... " + letter);
                 }
             }
+
             name = name.ToUpper();
             Console.WriteLine(name + " is just GRAND!");
+
+            if (convertedBirthday.Equals(today))
+                {
+                  Console.WriteLine("Today is your birthday!");
+                }
+                else 
+                {
+                if (convertedBirthday < today)
+                {
+                    convertedBirthday = convertedBirthday.AddYears(1);
+                }
+                 Console.WriteLine("Your Birthday is " + convertedBirthday.Subtract(today).Days + " days away.");
+                }
+
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
